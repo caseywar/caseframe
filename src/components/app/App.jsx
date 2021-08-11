85e7f40e1526b7b3284b96ca73eb07dd3fc023b0
Casey Warren <caseywar1017@gmail.com>
2021-08-11 01:38:14 -0400

    body component created

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import Header from '../app/pages/Header.jsx'
+import Body from '../app/pages/Body.jsx'
 
 export default function App() {
   return (
-    <Header />
+    <>
+    <Header /> 
+    <Body />
+    </>
   )
 }
--- src/components/app/pages/Header.jsx
+++ src/components/app/pages/Header.jsx
@@ -6,21 +6,33 @@ const Header = () => {
         <nav>
             <ul>
                 <li className={style.buttonContainer}>
-                    <a className={style.button}
-                      href="/about"
-                    >
-                        About Me
-                    </a>
-                    <a className={style.button}
-                      href="/about"
-                    >
-                        My Code
-                    </a>
-                    <a className={style.button}
-                      href="/about"
-                    >
-                        Resume
-                    </a>
+                    <p className={style.caseName}>
+                        CASEY WARREN
+                    </p>
+                    <div className={style.buttonBox}>
+                        <a className={style.button}
+                        href="/about"
+                        >
+                            About Me
+                        </a>
+                    </div>
+                    <div className={style.buttonBox}>
+
+                        <a className={style.button}
+                        href="/about"
+                        >
+                            My Code
+                        </a>
+                        </div>
+                        <div className={style.buttonBox}>
+
+                        <a className={style.button}
+                        href="/about"
+                        >
+                            Resume
+                        </a>
+                        </div>
+
                 </li>
                 
             </ul>
